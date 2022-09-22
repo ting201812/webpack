be2ea7b8675d8e2bef52da9111d380ede92319ba
ting201812 <2467476602@qq.com>
2022-09-22 11:30:13 +0800

    eslint资源配置

---
+++ 10_eslint/src/main.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: 张玉婷
+ * @Date: 2022-09-21 15:01:59
+ * @LastEditTime: 2022-09-22 11:24:12
+ * @LastEditors: 张玉婷
+ * @Description: any
+ * @FilePath: \webpack\10_eslint\src\main.js
+ */
+
+// 引入样式资源
+import './css/index.css';
+import './less/index.less'
+import './scss/index.scss'
+// 引入iconfont
+import './font/iconfont.css'
+
