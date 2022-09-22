baa28281415d8c88d8ac9629c6948f83661a564c
ting201812 <2467476602@qq.com>
2022-09-22 11:25:08 +0800

    修改输出文件目录

---
+++ 05_修改输出文件目录/src/main.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: 张玉婷
+ * @Date: 2022-09-21 15:01:59
+ * @LastEditTime: 2022-09-21 17:16:18
+ * @LastEditors: 张玉婷
+ * @Description: any
+ * @FilePath: \webpack\03_webpack样式资源2\src\main.js
+ */
+
+// 引入样式资源
+import './css/index.css';
+import './less/index.less'
+import './scss/index.scss'
