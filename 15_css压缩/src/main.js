19a498545091390a653b57355128ce30e143c049
ting201812 <2467476602@qq.com>
2022-09-28 18:08:02 +0800

    css压缩

---
+++ 15_css压缩/src/main.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: 张玉婷
+ * @Date: 2022-09-21 15:01:59
+ * @LastEditTime: 2022-09-22 10:25:47
+ * @LastEditors: 张玉婷
+ * @Description: any
+ * @FilePath: \webpack\07_处理字体图标资源\src\main.js
+ */
+
+// 引入样式资源
+import './css/index.css';
+import './less/index.less'
+import './scss/index.scss'
+// 引入iconfont
+import './font/iconfont.css'
