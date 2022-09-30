8d522b8144adeb2c1ea141e2417337d049913757
ting201812 <2467476602@qq.com>
2022-09-30 16:35:50 +0800

    01_sourceMap

---
+++ 16_高级/01_sourceMap/babel.config.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: 张玉婷
+ * @Date: 2022-09-22 13:42:32
+ * @LastEditTime: 2022-09-22 13:46:29
+ * @LastEditors: 张玉婷
+ * @Description: any
+ * @FilePath: \webpack\11_babel\babel.config.js
+ */
+// babel配置文件
+module.exports = {
+  presets: [
+    // 智能预设 编译ES6语法
+    '@babel/preset-env'
+  ],
+}
---
+++ 16_高级/01_sourceMap/src/main.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: 张玉婷
+ * @Date: 2022-09-21 15:01:59
+ * @LastEditTime: 2022-09-29 16:14:07
+ * @LastEditors: 张玉婷
+ * @Description: any
+ * @FilePath: \webpack\16_高级\01_sourceMap\src\main.js
+ */
+
+// 引入样式资源
+import './css/index.css';
+import './less/index.less'
+import './scss/index.scss'
+// 引入iconfont
+import './font/iconfont.css'
+
+import sum from './js/sum';
+
+sum(1, 3);
