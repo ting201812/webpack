2f3d9eb5eef6694ab331e74688b28db007664f39
ting201812 <2467476602@qq.com>
2022-10-08 13:56:27 +0800

    treeShaking

---
+++ 16_高级/07_tree_shaking/src/main.js
@@ -0,0 +1,29 @@
+/*
+ * @Author: 张玉婷
+ * @Date: 2022-09-21 15:01:59
+ * @LastEditTime: 2022-10-08 13:48:05
+ * @LastEditors: JunFan Hang
+ * @Description: any
+ * @FilePath: \webpack\16_高级\07_tree_shaking\src\main.js
+ */
+
+// 引入样式资源
+import "./css/index.css";
+import "./less/index.less";
+import "./scss/index.scss";
+// 引入iconfont
+import "./font/iconfont.css";
+
+import sum from "./js/sum";
+
+import { add } from "./js/index";
+
+sum(1, 7);
+add(1, 5);
+
+// js HMR配置
+// 判断是否支持HMR功能
+if (module.hot) {
+  // 需要热模块替换的js， 需要使用module.hot.accept配置
+  module.hot.accept("./js/sum");
+}
